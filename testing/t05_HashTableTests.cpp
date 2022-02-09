4849ec2100d0c0ad71bfccb87e11eed884947f70
cryten <cryten101@gmail.com>
2022-02-09 18:47:22 +0100

    test: add linked list hash table tests

--- testing/t05_HashTableTests.cpp
+++ testing/t05_HashTableTests.cpp
@@ -4,12 +4,21 @@
 class HashTableTests : public ::testing::Test {
 protected:
     void SetUp() override {
+        int value;
+
         emptyHashTable = new HashTable<int>(20);
         filledHashTable = new HashTable<int>(20);
-        int value = 99;
+        value = 99;
         filledHashTable->Insert(112, value);
         value = 112;
         filledHashTable->Insert(134, value);
+
+        emptyLLHashTable = new LLHashTable<int>(20);
+        filledLLHashTable = new LLHashTable<int>(20);
+        value = 99;
+        filledLLHashTable->Insert(112, value);
+        value = 112;
+        filledLLHashTable->Insert(134, value);
     }
 
     void TearDown() override {
@@ -18,28 +27,41 @@ protected:
 
         delete filledHashTable;
         filledHashTable = nullptr;
+
+        delete emptyLLHashTable;
+        emptyLLHashTable = nullptr;
+
+        delete filledLLHashTable;
+        filledLLHashTable = nullptr;
     }
 
     HashTable<int> *emptyHashTable;
     HashTable<int> *filledHashTable;
+    LLHashTable<int> *emptyLLHashTable;
+    LLHashTable<int> *filledLLHashTable;
 };
 
 TEST_F(HashTableTests, EmptyTableSize) {
     EXPECT_EQ(emptyHashTable->GetSize(), 23);
+    EXPECT_EQ(emptyLLHashTable->GetSize(), 23);
 }
 
 TEST_F(HashTableTests, Insert) {
     EXPECT_EQ(filledHashTable->GetTotalItems(), 2);
+    // TODO: also test filledLLHashTable
 }
 
 TEST_F(HashTableTests, Find) {
     int val = 99;
     EXPECT_TRUE(filledHashTable->Find(112, &val));
+    EXPECT_TRUE(filledLLHashTable->Find(112, &val));
 }
 
 TEST_F(HashTableTests, Delete) {
-    filledHashTable->Delete(112);
     int val = 99;
+    filledHashTable->Delete(112);
+    filledLLHashTable->Delete(112);
+    EXPECT_FALSE(filledLLHashTable->Find(112, &val));
     EXPECT_FALSE(filledHashTable->Find(112, &val));
 }
 
@@ -49,6 +71,8 @@ TEST_F(HashTableTests, StringHashing) {
     int stringHash1 = emptyHashTable->HashFunction(key1);
     int stringHash2 = emptyHashTable->HashFunction(key2);
     EXPECT_EQ(stringHash1, stringHash2);
+
+    // No StringHashing test for LLHashTable needed because of identical function
 }
 
 // TODO: Test collision handling
