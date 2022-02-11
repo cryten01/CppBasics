67a1228522c8781024cc9ba746c7a01bdc594af8
cryten <cryten101@gmail.com>
2022-02-11 19:35:35 +0100

    feat: add bubble, selection, insertion sort

--- src/04_Algorithms/Sort.h
+++ src/04_Algorithms/Sort.h
@@ -6,17 +6,76 @@
 
 // Comparison sorts
 
-// Counting sort
+void BubbleSort(std::vector<int> &array) {
+    // Determines the number of passes
+    for (int i = 0; i < array.size() - 1; i++) {
+        // Compare item against all non-sorted items
+        for (int j = 0; j < array.size() - i - 1; j++) {
+            // Swap items if current element is bigger than next (bubble up)
+            if (array[j] > array[j + 1]) {
+                int temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+void SelectionSort(std::vector<int> &array) {
+    int temp;
+    int min = 0;
+
+    for (int k = 0; k < array.size() - 1; k++) {
+        // Set min
+        min = k;
+
+        // Check for new min
+        for (int i = k + 1; i < array.size(); i++) {
+            if (array[i] < array[min]) {
+                min = i;
+            }
+        }
+
+        // Swap elements if element at min pos is smaller
+        if (array[k] > array[min]) {
+            temp = array[k];
+            array[k] = array[min];
+            array[min] = temp;
+        }
+    }
+}
+
+void InsertionSort(std::vector<int> &array) {
+    int temp;
+    int i = 0;
+
+    // Loop through all items except first
+    for (int k = 1; k < array.size(); k++) {
+        temp = array[k];
+        i = k;
+
+        // Loop down from k
+        while (i > 0 && array[i - 1] >= temp) {
+            array[i] = array[i - 1];
+            i--;
+        }
+
+        array[i] = temp;
+    }
+}
+
+// Counting sorts
+
 std::vector<int> CountingSort(std::vector<int> vec, int size) {
-    std::vector<int> result(size);
+    std::vector<int> frequency(size);
 
     // Loop through all elements
-    for (int element : vec) {
+    for (int element: vec) {
         // Increase count at position: element
-        result.at(element) += 1;
+        frequency.at(element) += 1;
     }
 
-    return result;
+    return frequency;
 }
 
 #endif //CPPBASICS_SORT_H
--- testing/ta02_SortTests.cpp
+++ testing/ta02_SortTests.cpp
@@ -1,6 +1,30 @@
 #include <gtest/gtest.h>
 #include "../src/04_Algorithms/Algorithms.h"
 
+class SortTests : public ::testing::Test {
+protected:
+    // void SetUp() override {}
+    // void TearDown() override {}
+
+    std::vector<int> vec{1, 4, 3, 2};
+    std::vector<int> toCompare{1, 2, 3, 4};
+};
+
+TEST_F(SortTests, BubbleSort) {
+    BubbleSort(vec);
+    EXPECT_EQ(vec, toCompare);
+}
+
+TEST_F(SortTests, SelectionSort) {
+    SelectionSort(vec);
+    EXPECT_EQ(vec, toCompare);
+}
+
+TEST_F(SortTests, InsertionSort) {
+    InsertionSort(vec);
+    EXPECT_EQ(vec, toCompare);
+}
+
 TEST(SortTests, CountingSort) {
     std::vector<int> vec{1, 1, 0, 2};
     std::vector<int> freq = CountingSort(vec, 3);
